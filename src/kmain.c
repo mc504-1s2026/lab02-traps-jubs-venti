26fa555abe1a5978c0acfe87abafa03e76a59ca1
jubs-venti <jubstudies28@gmail.com>
2026-07-14 21:14:19 -0300

    alterando kmainnmnnn

--- src/kmain.c
+++ src/kmain.c
@@ -33,7 +33,9 @@ void kmain()
     int cmdlen = 0;           // tamanho atual do comando no buffer
     char rx_buf_temp[256];    // buffer temporario para armazenar os dados lidos da serial
     
-	
+    char lixo[256];
+    serial_read(lixo);  
+	printk(LOG_INFO, "> ");
     while (1) {
         // le os dados da serial para o buffer temporario
         size_t bytes_lidos = serial_read(rx_buf_temp);
@@ -48,15 +50,20 @@ void kmain()
         for (size_t i = 0; i < bytes_lidos; i++) {
             char c = rx_buf_temp[i];
             
+            if (cmdlen == 0 && (c == '\r' || c == '\n')) {
+            // se o usuario apertou enter sem digitar nada, apenas imprima o prompt e ignore
+            printk(LOG_INFO, "\n> ");
+            continue;
+            }
 
             // verifica se o caractere lido eh enter
             if (c == '\r' || c == '\n') {
-                printk(LOG_INFO, "\n");
+                printk(LOG_INFO, "\r\n");
                 cmdbuf[cmdlen] = '\0'; 
 
                 // processa o comando digitado
                 if (strncmp(cmdbuf, "uptime", 6) == 0) {
-                    printk(LOG_INFO, "%llus\n", uptime_seconds);
+                    printk(LOG_INFO, "%llus\r\n", uptime_seconds);
                 } 
                 else if (strncmp(cmdbuf, "echo ", 5) == 0) {
                     printk(LOG_INFO, "%s\r\n", cmdbuf + 5); // imprime o que vem depois do "echo "
@@ -73,9 +80,9 @@ void kmain()
                     alarm_target = uptime_seconds + secs;
                     alarm_active = 1;
                 }
-                else if (cmdlen > 0) {
-                    printk(LOG_INFO, "Comando desconhecido: %s\n", cmdbuf);
-                }
+                //else if (cmdlen > 0) {
+                //    //printk(LOG_INFO, "Comando desconhecido: %s\n", cmdbuf);
+                //}
 
                 // reseta o buffer do comando e imprime o prompt novamente
                 cmdlen = 0;
